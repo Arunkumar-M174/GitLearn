ab0fbdaa53468a0171213363807a111f55228ac8
Arunkumar-M174 <Arunprince580@gmail.com>
2024-07-31 16:45:59 +0530

    Index on loop modify.

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ name=str(input("Enter your name:\n"))
 print("Welcome To Python Agian",name,"Do well")
 number=int(input("Enter the number:\n"))
 for count in range (0,number):
-    print("Hello",name ,count)
+    print("Hello",name ,count+1)
