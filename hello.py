c4a4dc526fb92d737ed99ab73c74103725bd9dac
Arunkumar-M174 <Arunprince580@gmail.com>
2024-07-31 15:59:35 +0530

    Greet N Times.

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
 name=str(input("Enter your name:\n"))
 print("Welcome To Python Agian",name,"Do well")
+number=int(input("Enter the number:\n"))
+for count in range (0,number):
+    print("Hello",name ,count)
