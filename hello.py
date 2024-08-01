7921b73f8a376f16216d5c1490347695e35ec2ba
Arunkumar-M174 <Arunprince580@gmail.com>
2024-08-01 10:47:27 +0530

    Owner Recognition.

--- hello.py
+++ hello.py
@@ -1,5 +1,17 @@
+# Import the necessary module
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file (if present)
+load_dotenv('./config.env')
 name=str(input("Enter your name:\n"))
 print("Welcome To Python Agian",name,"Do well")
 number=int(input("Enter the number:\n"))
 for count in range (0,number):
     print("Hello",name ,count+1)
+if(name!=os.getenv('OWNER')):
+    print("You are not the owner of this git")
+    
+else:
+    print("Welcome Owner")
+
